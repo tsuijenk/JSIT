72b5fdcf2b589c74b9976861e84fecc29aba960f
Jenkin Tsui <44915621+tsuijenk@users.noreply.github.com>
2025-04-23 18:11:56 -0700

    JSIT

---
+++ JSIT.m
@@ -0,0 +1,80 @@
+function [] = JSIT(fov,codebook,predicted_folder)
+
+    %% Example Script for running JSIT
+    
+    %% Load data
+    Ic = struct2cell(load(fov));
+    Cc = struct2cell(load(codebook));
+    I = double(Ic{:});
+    C = double(Cc{:});
+    
+    
+    %% Crop image to reduce RAM requirement
+    cropSize = 400;
+    block_x = 3;
+    block_y = 3;
+    Ic = I((block_x*cropSize)+1:(block_x+1)*cropSize,(block_y*cropSize)+1:(block_y+1)*cropSize,:);
+    sz1 = size(Ic,1);
+    sz2 = size(Ic,2);
+    
+    %% Set parameters
+    sf = 3; %Resolution scale factor 
+    sigma = 1.25; %Width of microscope PSF
+    s1 = 40; %Patch size
+    s2 = 40;
+    kmax = 10; %FISTA iterations
+    lambda = 75; %Regularization parameter
+    k = 1; %Row-sparsity of estimated X
+    t = 0; %Hard-threshold on estimated X
+    
+    %% Prepare matrices for FISTA
+    A = getPsfMat2(40*sf,sf,sigma);
+    %Pre-compute largest Eigenvalues of CC' and AA'
+    K = C*C';
+    [~,Sk,~] = svd(K);
+    eK = Sk(1,1);
+    Ma = A*A';
+    [~,Sm,~] = svd(Ma);
+    eM = Sm(1,1);
+    A = sparse(A);
+    
+    %% Pre-process data
+    Ih = imgaussfilt(Ic,0.5)-imgaussfilt(Ic,3);
+    Ystack = double(makeYstack(Ih,s1,s2));
+    nYs = size(Ystack,3);
+    Xstack = zeros(nYs,(s1.*sf)^2,size(C,1));
+    
+    %% Run FISTA
+    for x = 1:nYs
+        Xstack(x,:,:) = codebookFISTA(A,C,Ystack(:,:,x),lambda,kmax,'SGL',0.5,[],eK,eM);
+    end
+    
+    %% Post-process decoding results
+    d1 = sz1./s1;
+    d2 = sz2./s2;
+    X = processXstack(Xstack,d1,d2,s1*sf,s1*sf);
+    Xf = enforceSparsity2(X,k,t);
+    xIm = reshape(sum(Xf,2),[sz1*sf,sz2*sf]);
+    Icv = reshape(Ic,[sz1*sz2,16]);
+    iv = vecnorm(double(Icv)',2)';
+    iIm = reshape(iv,[sz1,sz2]);
+    iIm = imresize(iIm,sf);
+    dIm = X2dIm(Xf,sz1*sf,sz2*sf);
+    q = dIm2q_ex(dIm,iIm,xIm,2,C);
+    q(:,1:2) = q(:,1:2)./sf;
+    %% Adaptive filtering
+    nA = 10;
+    nI = 10;
+    nX = 10;
+    nC = 115;
+    nB = 25;
+    tgt = 0.05;
+    [qqt,qqct] = getThresholdHist(q,nA,nI,nX,nC,nB,tgt);
+    
+    
+    imwrite(dIm,fullfile(predicted_folder,'unfiltered_predictions.tiff'));
+    imwrite(iIm,fullfile(predicted_folder,"spot_predictions.tiff"));
+    writematrix(qqt,fullfile(predicted_folder,"barcodes_with_blanks.csv"));
+    writematrix(qqct,fullfile(predicted_folder,"barcodes_wo_blanks.csv"));
+
+end
---
+++ aligned_stack_jsit.py
@@ -0,0 +1,66 @@
+from scipy.io import savemat
+import matplotlib.pyplot as plt
+import os
+import re
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from tifffile import imread
+from concurrent.futures import ThreadPoolExecutor
+from tqdm.auto import tqdm
+from argparse import ArgumentParser
+
+
+'''
+    Change format from (round,x,y) to (x,y,round)
+'''
+def aligned_jsit(img:str,out:str):
+    i = imread(img)
+    oi = np.stack(i,axis=-1)
+    savemat(f'{out}.mat',{'ans':oi})
+    return f"Wrote {os.path.split(img)[1]}"
+'''
+    Helper to convert C1E1 barcode strings to codebook matrix
+'''
+def split_barcode(barcode:str):
+    return [int(bit) for bit in barcode.split()]
+
+
+def main(img_folder:str,codebook:str,output_folder:str,*, workers:int = 7):
+    if not os.path.exists(output_folder): os.mkdir(output_folder)
+    #save Code Book Skip first for rows bc of codebook weirdness
+    c = pd.read_csv(
+        codebook,skiprows=4,header = None
+    )
+    barcodes = c.iloc[:,2].apply(split_barcode)
+    barcodes = np.array([np.array(barcode) for barcode in barcodes])
+    savemat(
+        os.path.join(output_folder,'codebook.mat'),
+        {'ans':barcodes}
+    )
+    print("Converting FOV's to JSIT format")
+    imgs = os.listdir(img_folder)
+    with ThreadPoolExecutor(max_workers=workers) as exec:
+        jobs  = []
+        for fov in imgs:
+            img = os.path.join(img_folder,fov)
+            out = os.path.join(output_folder,fov)
+            jobs.append(exec.submit(aligned_jsit,img,out))
+        for job in jobs:
+            print(job.result())
+        
+        
+if __name__ == "__main__":
+    cli = True
+    if cli: 
+        parser = ArgumentParser()
+        parser.add_argument('-i',type=str, help="Folder for Restacked Aligned images")
+        parser.add_argument('-c', type=str, help='path to codebook.  Expects similar format to C1E1_codebook.csv')
+        parser.add_argument('-o', default="JSIT_input",type=str, help="Folder to write images and codebook to")
+        args = parser.parse_args()
+        main(args.i,args.c,args.o)
+    else:
+        im ="/home/isaac/dev/molonc/gsc/scratch/restacked_aligned"
+        cb = "/home/isaac/dev/molonc/gsc/scratch/parameters/codebooks/C1E1_codebook.csv"
+        main(im,cb,'xp6873')
+
